6b9d50ed068ceb1f49de58451e676f3cbf2450a9
yhoungdev <yhoungdev@gmail.com>
2024-03-22 10:02:19 +0100

    finished up fotter

--- src/app/_components/organisms/footer.tsx
+++ src/app/_components/organisms/footer.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   FaFacebook,
   FaTwitter,
@@ -7,131 +8,107 @@ import {
 } from "react-icons/fa";
 import Brandmark from "../atom/brandmark";
 import Button from "../atom/button";
+import Container from "../template/container";
 
 const Footer = () => {
+  const footerSections = {
+    Homeowners: [
+      { href: "/post-job", text: "Post a Job" },
+      { href: "/how-it-works", text: "How it works" },
+      { href: "/contact-us", text: "Contact Us" },
+      { href: "/help-faqs", text: "Help & FAQ's" },
+      { href: "/user-agreement", text: "User Agreement" },
+    ],
+    Professionals: [
+      { href: "/sign-up", text: "Sign Up" },
+      { href: "/blog", text: "Blog" },
+      { href: "/contact-us", text: "Contact Us" },
+      { href: "/help-faqs", text: "Help & FAQ's" },
+      { href: "/user-agreement", text: "User Agreement" },
+    ],
+    Loggworks: [
+      { href: "/about-us", text: "About us" },
+      { href: "/careers", text: "Careers" },
+      { href: "/partners", text: "Partners" },
+      { href: "/affiliate", text: "Affiliate" },
+    ],
+  };
+
+  const socialMediaLinks = [
+    { href: "https://facebook.com", icon: <FaFacebook />, label: "Facebook" },
+    { href: "https://twitter.com", icon: <FaTwitter />, label: "Twitter" },
+    {
+      href: "https://instagram.com",
+      icon: <FaInstagram />,
+      label: "Instagram",
+    },
+    { href: "https://youtube.com", icon: <FaYoutube />, label: "YouTube" },
+    { href: "https://linkedin.com", icon: <FaLinkedin />, label: "LinkedIn" },
+  ];
+
   return (
-    <footer className="bg-black text-gray-300">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="grid grid-cols-2 md:grid-cols-6 gap-8">
-          <div className="col-span-2">
-            <div className="hidden md:flex flex-col item w-auto mr-auto">
+    <footer className="bg-black text-gray-300 md:py-[4em]">
+      <Container>
+      <div className="max-w-7xl px-4 sm:px-6 lg:px-8 py-12 ">
+        <div className="flex justify-between md:flex-row flex-wrap">
+          <div className="flex flex-col gap-[3em] md:flex-row md:justify-between w-full">
+            <div className="mb-8 hidden md:block">
               <Brandmark />
-              <div className="flex flex-col gap-[16px] mt-[33px] w-auto mr-auto">
-                <button className="border-2 rounded border-gray-300 text-gray-300 py-[7px] px-[8px]">
-                  Become a Professional
-                </button>
-                <button className="border-2 rounded border-gray-300 text-gray-300 py-[7px] px-[8px]">
-                  Post a job
-                </button>
+              <div className="mt-8 flex  flex-col w-[250px]">
+               <Button className="mb-4 w-full">Become a Professional</Button>
+                <Button className="border border-red-white bg-black text-white">Post a job</Button>
+              </div>
+            </div>
+            <div className="md:w-[90%] w-full flex-col  md:flex md:justify-end">
+              <div className=" flex justify-between flex-wrap gap-[2.5em] flex-col md:flex-row">
+                {Object.entries(footerSections).map(([sectionName, links]) => (
+                  <div key={sectionName} className="">
+                    <h5 className="text-white font-bold mb-5">{sectionName}</h5>
+                    <ul className="text-sm space-y-2">
+                      {links.map((link) => (
+                        <li key={link.href}>
+                          <a href={link.href} className="hover:underline">
+                            {link.text}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+                <div className="col-span-1">
+                  <h5 className="text-white font-bold mb-3">Follow us</h5>
+                  <ul className="flex  gap-3 flex-col ml-0">
+                    {socialMediaLinks.map((social) => (
+                      <li key={social.label} className="ml-0">
+                        <a
+                          href={social.href}
+                          className="hover:text-gray-500  gap-1"
+                          aria-label={social.label}
+                        >
+                          <div className="flex gap-1">
+                            <div>{social.icon}</div>
+                            <small>{social.label}</small>
+                          </div>
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-span-1">
-            <h5 className="text-white font-bold mb-5">Homeowners</h5>
-            <ul className="text-sm [&_li]:mb-3">
-              <li>
-                <a href="/post-job">Post a Job</a>
-              </li>
-              <li>
-                <a href="/how-it-works">How it works</a>
-              </li>
-              <li>
-                <a href="/contact-us">Contact Us</a>
-              </li>
-              <li>
-                <a href="/help-faqs">Help & FAQ&quot;s</a>
-              </li>
-              <li>
-                <a href="/user-agreement">User Agreement</a>
-              </li>
-            </ul>
-          </div>
 
-          <div className="col-span-1">
-            <h5 className="text-white font-bold mb-5">Professionals</h5>
-            <ul className="text-sm [&_li]:mb-3">
-              <li>
-                <a href="/sign-up">Sign Up</a>
-              </li>
-              <li>
-                <a href="/blog">Blog</a>
-              </li>
-              <li>
-                <a href="/contact-us">Contact Us</a>
-              </li>
-              <li>
-                <a href="/help-faqs">Help & FAQ&quot;s</a>
-              </li>
-              <li>
-                <a href="/user-agreement">User Agreement</a>
-              </li>
-            </ul>
-          </div>
+          <div className=" my-[3em] w-full block md:hidden">
 
-          <div className="col-span-1">
-            <h5 className="text-white font-bold mb-5">Loggworks</h5>
-            <ul className="text-sm [&_li]:mb-3">
-              <li>
-                <a href="/about-us">About us</a>
-              </li>
-              <li>
-                <a href="/careers">Careers</a>
-              </li>
-              <li>
-                <a href="/partners">Partners</a>
-              </li>
-              <li>
-                <a href="/affiliate">Affiliate</a>
-              </li>
-            </ul>
-          </div>
+              <div className="flex  flex-col">
+                <Button className="mb-4">Become a Professional</Button>
+                <Button className="border border-red-white bg-black text-[#FAFAFA">Post a job</Button>
 
-          <div className="col-span-1">
-            <h5 className="text-white font-bold mb-3">Follow us</h5>
-            <ul className="flex space-x-4">
-              <li>
-                <a href="https://facebook.com">
-                  <FaFacebook />
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li>
-                <a href="https://instagram.com">
-                  <FaInstagram />
-                </a>
-              </li>
-              <li>
-                <a href="https://youtube.com">
-                  <FaYoutube />
-                </a>
-              </li>
-              <li>
-                <a href="https://linkedin.com">
-                  <FaLinkedin />
-                </a>
-              </li>
-            </ul>
-          </div>
+              </div>
+            </div>
         </div>
-
-        <div className="flex md:hidden  flex-col item w-full ">
-          <div className="flex flex-col w-full gap-[16px] mt-[33px] mr-auto">
-            <Button className="w-full">
-              Become a Professional
-            </Button>
-            <button className="border-2 rounded border-gray-300 text-gray-300 py-[7px] px-[8px]">
-              Post a job
-            </button>
-          </div>
-        </div>
-       
-
-        <div className="border-t border-gray-700 py-8 mt-8 flex flex-col md:flex-row justify-between items-center text-white text-sm">
-          <p>©Copyright 2024 loggworks Ltd. All Rights Reserved</p>
+        <div className="border-t text-center md:text-left mt-[2em] md:mt-[8em] border-gray-700 py-8  flex flex-col md:flex-row justify-between items-center text-white text-sm">
+          <p>Copyright © 2005 - {new Date().getFullYear()} {""}loggworks Ltd. All Rights Reserved</p>
           <div className="flex items-center justify-between gap-3 mt-4 md:mt-0 text-center">
             <a
               href="/terms-of-use"
@@ -157,6 +134,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
+      </Container>
     </footer>
   );
 };
--- src/app/_components/organisms/home/exploreCategories.tsx
+++ src/app/_components/organisms/home/exploreCategories.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import CategoryPanel from "../../atom/categoryPanel";
 import { ServiceCategories } from "@/app/utils/data";
@@ -13,36 +12,40 @@ const ExploreCategories = () => {
           Explore service by categories
         </h1>
 
-      
         <div className="pb-[66px]">
           <div className="flex items-center justify-between">
-          <h1 className="pb-3 text-[19px] font-bold">Trades Men</h1>
-          <div className="flex items-center gap-1 font-medium text-sm md:text-md text-accent cursor-pointer">
-            <FiPlus/>
-            <h1>View All</h1>
-          </div>
+            <h1 className="pb-3 text-[19px] font-bold">Trades Men</h1>
+            <div className="flex items-center gap-1 font-medium text-sm md:text-md text-accent cursor-pointer">
+              <FiPlus />
+              <h1>View All</h1>
+            </div>
           </div>
           <div className="flex flex-nowrap gap-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-6 md:overflow-visible">
             {ServiceCategories.tradesMen.map((category, key) => (
-              <div key={key} className="flex-shrink-0 min-w-[calc(50%-1rem)] md:min-w-full md:w-auto">
+              <div
+                key={key}
+                className="flex-shrink-0 min-w-[calc(50%-1rem)] md:min-w-full md:w-auto"
+              >
                 <CategoryPanel data={category} assetRepo="tradesMen/" />
               </div>
             ))}
           </div>
         </div>
 
-       
         <div className="pb-3">
-        <div className="flex items-center justify-between">
-          <h1 className="pb-3 text-[19px] font-bold">Cleaning</h1>
-          <div className="flex items-center gap-1 font-medium text-sm md:text-md text-accent cursor-pointer">
-            <FiPlus/>
-            <h1>View All</h1>
-          </div>
+          <div className="flex items-center justify-between">
+            <h1 className="pb-3 text-[19px] font-bold">Cleaning</h1>
+            <div className="flex items-center gap-1 font-medium text-sm md:text-md text-accent cursor-pointer">
+              <FiPlus />
+              <h1>View All</h1>
+            </div>
           </div>
           <div className="flex flex-nowrap gap-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-6 md:overflow-visible">
             {ServiceCategories.cleaning.map((category, key) => (
-              <div key={key} className="flex-shrink-0 min-w-[calc(50%-1rem)] md:min-w-full md:w-auto">
+              <div
+                key={key}
+                className="flex-shrink-0 min-w-[calc(50%-1rem)] md:min-w-full md:w-auto"
+              >
                 <CategoryPanel data={category} assetRepo="cleaning/" />
               </div>
             ))}
