a1696b9911fd1554c5a223bf91072cd1132d3165
yhoungdev <yhoungdev@gmail.com>
2024-03-22 10:11:29 +0100

    update

--- src/app/_components/organisms/footer.tsx
+++ src/app/_components/organisms/footer.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   FaFacebook,
   FaTwitter,
@@ -49,91 +49,106 @@ const Footer = () => {
   return (
     <footer className="bg-black text-gray-300 md:py-[4em]">
       <Container>
-      <div className="max-w-7xl px-4 sm:px-6 lg:px-8 py-12 ">
-        <div className="flex justify-between md:flex-row flex-wrap">
-          <div className="flex flex-col gap-[3em] md:flex-row md:justify-between w-full">
-            <div className="mb-8 hidden md:block">
-              <Brandmark />
-              <div className="mt-8 flex  flex-col w-[250px]">
-               <Button className="mb-4 w-full">Become a Professional</Button>
-                <Button className="border border-red-white bg-black text-white">Post a job</Button>
+        <div className="max-w-7xl px-4 sm:px-6 lg:px-8 py-12 ">
+          <div className="flex justify-between md:flex-row flex-wrap">
+            <div className="flex flex-col gap-[3em] md:flex-row md:justify-between w-full">
+              <div className="mb-8 hidden md:block">
+                <Brandmark />
+                <div className="mt-8 flex  flex-col w-[250px]">
+                  <Button className="mb-4 w-full">Become a Professional</Button>
+                  <Button className="border border-red-white bg-black text-white">
+                    Post a job
+                  </Button>
+                </div>
               </div>
-            </div>
-            <div className="md:w-[90%] w-full flex-col  md:flex md:justify-end">
-              <div className=" flex justify-between flex-wrap gap-[2.5em] flex-col md:flex-row">
-                {Object.entries(footerSections).map(([sectionName, links]) => (
-                  <div key={sectionName} className="">
-                    <h5 className="text-white font-bold mb-5">{sectionName}</h5>
-                    <ul className="text-sm space-y-2">
-                      {links.map((link) => (
-                        <li key={link.href}>
-                          <a href={link.href} className="hover:underline">
-                            {link.text}
+              <div className="md:w-[90%] w-full flex-col  md:flex md:justify-end">
+                <div className=" flex justify-between flex-wrap gap-[2.5em] flex-col md:flex-row">
+                  {Object.entries(footerSections).map(
+                    ([sectionName, links]) => (
+                      <div key={sectionName} className="">
+                        <h5 className="text-white font-bold mb-5">
+                          {sectionName}
+                        </h5>
+                        <ul className="text-sm space-y-2">
+                          {links.map((link) => (
+                            <li key={link.href}>
+                              <a href={link.href} className="hover:underline">
+                                {link.text}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ),
+                  )}
+                  <div className="col-span-1">
+                    <h5 className="text-white font-bold mb-3">Follow us</h5>
+                    <ul className="flex  gap-3 flex-col ml-0">
+                      {socialMediaLinks.map((social) => (
+                        <li key={social.label} className="ml-0">
+                          <a
+                            href={social.href}
+                            className="hover:text-gray-500  gap-1"
+                            aria-label={social.label}
+                          >
+                            <div className="flex gap-1">
+                              <div>{social.icon}</div>
+                              <small>{social.label}</small>
+                            </div>
                           </a>
                         </li>
                       ))}
                     </ul>
                   </div>
-                ))}
-                <div className="col-span-1">
-                  <h5 className="text-white font-bold mb-3">Follow us</h5>
-                  <ul className="flex  gap-3 flex-col ml-0">
-                    {socialMediaLinks.map((social) => (
-                      <li key={social.label} className="ml-0">
-                        <a
-                          href={social.href}
-                          className="hover:text-gray-500  gap-1"
-                          aria-label={social.label}
-                        >
-                          <div className="flex gap-1">
-                            <div>{social.icon}</div>
-                            <small>{social.label}</small>
-                          </div>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className=" my-[3em] w-full block md:hidden">
-
+            <div className=" my-[3em] w-full block md:hidden">
               <div className="flex  flex-col">
                 <Button className="mb-4">Become a Professional</Button>
-                <Button className="border border-red-white bg-black text-[#FAFAFA">Post a job</Button>
-
+                <Button className="border border-red-white bg-black text-[#FAFAFA">
+                  Post a job
+                </Button>
               </div>
             </div>
-        </div>
-        <div className="border-t text-center md:text-left mt-[2em] md:mt-[8em] border-gray-700 py-8  flex flex-col md:flex-row justify-between items-center text-white text-sm">
-          <p>Copyright © 2005 - {new Date().getFullYear()} {""}loggworks Ltd. All Rights Reserved</p>
-          <div className="flex items-center justify-between gap-3 mt-4 md:mt-0 text-center">
-            <a
-              href="/terms-of-use"
-              className="hover:opacity-70 transition-opacity"
-            >
-              Terms of Use
-            </a>
-            <div className="h-6   border-l border-gray-600" />
-            <a
-              href="/privacy-policy"
-              className="hover:opacity-70 transition-opacity"
-            >
-              Privacy Policy
-            </a>
-            <div className="h-6  border-l border-gray-600" />
-            <a href="/cookies" className="hover:opacity-70 transition-opacity">
-              Cookies
-            </a>
-            <div className="h-6  border-l border-gray-600" />
-            <a href="/sitemap" className="hover:opacity-70 transition-opacity">
-              Sitemap
-            </a>
+          </div>
+          <div className="border-t text-center md:text-left mt-[2em] md:mt-[8em] border-gray-700 py-8  flex flex-col md:flex-row justify-between items-center text-white text-sm">
+            <p>
+              Copyright © 2005 - {new Date().getFullYear()} {""}loggworks Ltd.
+              All Rights Reserved
+            </p>
+            <div className="flex items-center justify-between gap-3 mt-4 md:mt-0 text-center">
+              <a
+                href="/terms-of-use"
+                className="hover:opacity-70 transition-opacity"
+              >
+                Terms of Use
+              </a>
+              <div className="h-6   border-l border-gray-600" />
+              <a
+                href="/privacy-policy"
+                className="hover:opacity-70 transition-opacity"
+              >
+                Privacy Policy
+              </a>
+              <div className="h-6  border-l border-gray-600" />
+              <a
+                href="/cookies"
+                className="hover:opacity-70 transition-opacity"
+              >
+                Cookies
+              </a>
+              <div className="h-6  border-l border-gray-600" />
+              <a
+                href="/sitemap"
+                className="hover:opacity-70 transition-opacity"
+              >
+                Sitemap
+              </a>
+            </div>
           </div>
         </div>
-      </div>
       </Container>
     </footer>
   );
--- src/app/_components/organisms/home/mobileDownload.tsx
+++ src/app/_components/organisms/home/mobileDownload.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import Container from "../../template/container";
 
 interface ImobileDownload {}
 
 const MobileDownload: React.FC<ImobileDownload> = () => {
   return (
-    <div className="container py-18 px-4 sm:px-6 lg:px-8 my-[5em]">
+    <Container>
+      <div className=" py-18 px-4 sm:px-6 lg:px-8 my-[5em] ">
       <div className="rounded-2xl bg-[#2E2E2E] grid grid-rows-1 overflow-hidden sm:h-[474px] pt-[2em]">
         <div className="grid grid-cols-1 sm:grid-cols-2 w-full h-full">
           <div className="flex justify-center items-end px-10 sm:px-10 mt-auto mb-10 sm:mb-0 sm:h-full">
@@ -18,14 +20,16 @@ const MobileDownload: React.FC<ImobileDownload> = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col justify-center items-start text-white px-10 py-8">
-            <h3 className="font-bold text-3xl sm:text-5xl leading-snug mb-4">
-              Access Loggworks <br /> on your mobile phone
-            </h3>
-            <p className="text-base sm:text-lg text-[#DFDFDF] mb-6">
-              Enjoy the full experience of Loggworks on our mobile app. What’s
-              more? You get to enjoy all of our services conveniently.
-            </p>
+          <div className="flex flex-col justify-center items-start text-white  px-10 py-8   ">
+            <div className="w-full md:w-auto ">
+              <h3 className="font-bold text-2xl sm:text-4xl leading-snug mb-4">
+                Access Loggworks <br /> on your mobile phone
+              </h3>
+              <p className="text-base sm:text-lg text-[#DFDFDF] mb-6 ">
+                Enjoy the full experience of Loggworks on our mobile app. What
+                more? You get to enjoy all of our services conveniently.
+              </p>
+            </div>
             <button className="bg-white w-full md:w-[30%] text-black font-bold py-3 px-7 rounded opacity-70">
               Coming soon
             </button>
@@ -33,6 +37,7 @@ const MobileDownload: React.FC<ImobileDownload> = () => {
         </div>
       </div>
     </div>
+    </Container>
   );
 };
 
